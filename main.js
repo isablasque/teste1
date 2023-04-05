54eddd76dea6e01795e0fa1cac6b3e43972be286
isablasque <129889752+isablasque@users.noreply.github.com>
2023-04-05 11:57:22 -0300

    djissssssssssssss

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function showcolor()
 function showname() 
 {
     let usuário = document.getElementById("nome").value;
-    document.getElementById("mensagem").innerText= "Meu nome é: " + usuário;
+    document.getElementById("mensagem").innerHTML= "Meu nome é: " + usuário;
     document.getElementById("nome").value = "";
 }
+function soma(a = required(), b = required()) { return a + b; } console. log(soma(1, 2));
