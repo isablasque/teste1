7dc60034503765f54c05e262481ac10daedc142e
isablasque <129889752+isablasque@users.noreply.github.com>
2023-04-05 12:22:38 -0300

    aaaaaaaaaaaa

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ document.getElementById("botao").addEventListener("click", showname)
 
 document.getElementById("btn-soma").addEventListener("click", soma)
 
+document.getElementById("btn-divisao").addEventListener("click", divisao)
+
+document.getElementById("btn-vezes").addEventListener("click", multiplicaçao)
+
+document.getElementById("btn-menos").addEventListener("click", subtraçao)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -21,5 +27,12 @@ function soma()
     let numero1 = document.getElementById("num1").value;
     let numero2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML= parseFloat(numero1) + parseFloat(numero2)
+}
+
+function divisao() 
+{
+    let numero1 = document.getElementById("num1").value;
+    let numero2 = document.getElementById("num2").value;
+    document.getElementById("resultado2").innerHTML= parseFloat(numero1) / parseFloat(numero2)
 }
 
