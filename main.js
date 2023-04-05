71198ddf455d040c019dac313ea15c367d981752
isablasque <129889752+isablasque@users.noreply.github.com>
2023-04-05 12:12:44 -0300

    plssssssssssss

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("click", showcolor);
 
 document.getElementById("botao").addEventListener("click", showname)
 
+document.getElementById("btn-soma").addEventListener("click", soma)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -13,4 +15,11 @@ function showname()
     document.getElementById("mensagem").innerHTML= "Meu nome é: " + usuário;
     document.getElementById("nome").value = "";
 }
-function soma(a = required(), b = required()) { return a + b; } console. log(soma(1, 2));
+
+function soma() 
+{
+    let numero1 = document.getElementById("num1").value;
+    let numero2 = document.getElementById("num2").value;
+    document.getElementById("resultado1").innerHTML= parseFloat(numero1) + parseFloat(numero2)
+}
+
